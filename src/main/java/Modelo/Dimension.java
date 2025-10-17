0f083a36527798125348baacddc9239f3e01433e
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-16 22:14:12 -0500

    Añadi el codigo de la clase Dimension

--- src/main/java/Modelo/Dimension.java
+++ src/main/java/Modelo/Dimension.java
@@ -9,5 +9,32 @@ package Modelo;
  * @author migue
  */
 public class Dimension {
-    
+       private double ancho;
+    private double alto;
+
+    public Dimension(double ancho, double alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+
+    public String toString() {
+        return "Ancho: " + ancho + ", Alto: " + alto;
+    } 
 }
